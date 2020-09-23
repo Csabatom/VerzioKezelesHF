c240c0ddf5bc49a2fe368a14e4d222dffb106ff7
Nyirom2 <nyirocsabi@gmail.com>
2020-09-23 13:56:53 +0200

    Felhasználó által megadott tömb hossza, és a tömbben megadott szám átlaga

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -27,6 +27,27 @@ namespace VerzioKezelesHF
             }
             Console.WriteLine("A beírt számok átlaga: {0}", szum/tizSzam.Length);
             Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Adja meg hogy hány elemes legyen a tömb");
+            int tombHossz = int.Parse(Console.ReadLine());
+            int[] szamok = new int[tombHossz];
+            double szumSajatTomb = 0;
+            for (int i = 1; i <= szamok.Length; i++)
+            {
+                Console.Write("Adja meg a {0} számot: ", i);
+                try
+                {
+                    szamok[i - 1] = int.Parse(Console.ReadLine());
+                    szumSajatTomb += szamok[i - 1];
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hibás értéket adott meg!");
+                }
+            }
+            Console.WriteLine("A beírt számok átlaga: {0}", szumSajatTomb / szamok.Length);
+            Console.ReadKey();
         }
     }
 }
