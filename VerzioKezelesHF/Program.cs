7adc5dd3f0a17f8986d0261156a04a8ede53b9e1
Nyirom2 <nyirocsabi@gmail.com>
2020-09-23 13:15:10 +0200

    Felhasználó által megadott tíz szám átlaga

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,7 +10,23 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-
+            int[] tizSzam = new int[10];
+            double szum = 0;
+            for (int i = 1; i <= tizSzam.Length; i++)
+            {
+                Console.Write("Adja meg a {0} számot: ", i);
+                try
+                {
+                    tizSzam[i-1] = int.Parse(Console.ReadLine());
+                    szum += tizSzam[i-1];
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hibás értéket adott meg!");
+                }
+            }
+            Console.WriteLine("A beírt számok átlaga: {0}", szum/tizSzam.Length);
+            Console.ReadKey();
         }
     }
 }
